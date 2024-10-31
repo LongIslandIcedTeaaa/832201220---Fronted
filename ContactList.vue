a3854c37370858ecb03cf0b97c08cb12732fa86a
LongIslandIcedTeaaa <130127349+LongIslandIcedTeaaa@users.noreply.github.com>
2024-10-31 20:50:47 +0800

    Create ContactList.vue

---
+++ ContactList.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- 联系人列表结构 -->
+</template>
+
+<script>
+export default {
+  props: ['contacts'],
+  methods: {
+    deleteContact(contactId) {
+      // 删除联系人的逻辑
+    }
+  }
+};
+</script>
