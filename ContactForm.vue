645cfdbaf4f9711fed3b27804b3ba90456d5226f
LongIslandIcedTeaaa <130127349+LongIslandIcedTeaaa@users.noreply.github.com>
2024-10-31 20:45:51 +0800

    Create ContactForm.vue

---
+++ ContactForm.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- 表单结构 -->
+</template>
+
+<script>
+export default {
+  props: ['editingContact'],
+  data() {
+    return {
+      // 表单数据
+    };
+  },
+  methods: {
+    submitForm() {
+      // 提交表单的逻辑
+    }
+  }
+};
+</script>
